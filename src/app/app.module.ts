965412bc3caa6f1e1eeb957bae6f05d17f9c5b04
V3nd3 <102618483+V3nd3@users.noreply.github.com>
2022-08-11 19:16:47 -0300

    primer commit
    parte basica del front del portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './Componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './Componentes/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './Componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './Componentes/educacion/educacion.component';
+import { HabilidadesComponent } from './Componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './Componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
